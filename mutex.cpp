58ee2c04c7a13f13fc15e6efff7104b0821a6056
Oleg <olezhkabobrov@gmail.com>
2021-09-18 17:54:07 +0300

    First version

--- mutex.cpp
+++ mutex.cpp
@@ -1,18 +1,105 @@
 #include "mutex.h"
-#include <thread>
-#include <unordered_map>
 #include <iostream>
+#include <cassert>
 
-void my::mutex::lock() {}
+void my::lock_graph::add_vertex(vertex *v) {
+    holded_mutexes[std::this_thread::get_id()].insert(v);
+}
 
-void my::mutex::unlock() {}
+void my::lock_graph::delete_vertex(vertex *v) {
+    holded_mutexes[std::this_thread::get_id()].erase(v);
+}
 
-bool my::mutex::try_lock() {}
+void my::lock_graph::add_edges(vertex *v) {
+    for (vertex* vout : holded_mutexes[std::this_thread::get_id()]) {
+        vout->next_vertex = v;
+    }
+}
+
+void my::lock_graph::delete_edges() {
+    for (vertex *v : holded_mutexes[std::this_thread::get_id()]) {
+        v->next_vertex = nullptr;
+    }
+}
+
+bool my::lock_graph::loop_found(vertex *starting_vertex) const {
+    vertex* current_vertex = starting_vertex->next_vertex;
+    while(current_vertex != starting_vertex) {
+        if (!current_vertex) {
+            return false;
+        }
+        current_vertex = current_vertex->next_vertex;
+    }
+    return true;
+}
+
+my::lock_graph my::mutex::graph = lock_graph();
+
+my::mutex::mutex() {
+    v = new vertex();
+}
+
+my::mutex::~mutex() {
+    delete v;
+}
+
+void my::mutex::lock() {
+    if (m.try_lock()) {
+        graph.delete_edges();
+        graph.add_vertex(v);
+    } else {
+        graph.add_edges(v);
+        if (graph.loop_found(v)) {
+            std::cout << "DEADLOCK\n";
+            assert(0);
+        }
+    }
+}
+
+void my::mutex::unlock() {
+    graph.delete_vertex(v);
+    m.unlock();
+}
+
+bool my::mutex::try_lock() {
+    if (m.try_lock()) {
+        graph.delete_edges();
+        graph.add_vertex(v);
+        return true;
+    }
+    return false;
+}
+
+my::mutex m1, m2;
+void func() {
+    for (int i = 0; i < 10000; ++i) {
+        m1.lock();
+        m2.lock();
+        m1.unlock();
+        m2.unlock();
+    }
+}
+
 
 int main() {
-    std::unordered_map<std::thread::id, int> mp;
-    mp[std::this_thread::get_id()] = 5;
-    for (auto x : mp) {
-        std::cout << x.first << ' ' << x.second;
-    }
+
+
+    std::thread t1([&]() {
+        for (int i = 0; i < 10000; ++i) {
+            m1.lock();
+            m2.lock();
+            m1.unlock();
+            m2.unlock();
+        }
+    });
+    std::thread t2([&]() {
+      for (int i = 0; i < 10000; ++i) {
+          m2.lock();
+          m1.lock();
+          m2.unlock();
+          m1.unlock();
+      }
+    });
+    t1.join();
+    t2.join();
 }
--- mutex.h
+++ mutex.h
@@ -1,20 +1,38 @@
 #pragma once
 #include <mutex>
 #include <unordered_map>
-#include <vector>
+#include <set>
+#include <thread>
 
 namespace my {
-class graph {
+struct vertex {
+    vertex *next_vertex = nullptr;
+};
 
+class lock_graph {
+    std::unordered_map<std::thread::id,
+                       std::set<vertex*>> holded_mutexes;
+
+public:
+    void add_vertex(vertex *);
+    void delete_vertex(vertex *);
+    void add_edges(vertex *);
+    void delete_edges();
+    bool loop_found(vertex *) const;
 };
 
 class mutex {
-    std::mutex mutex;
+    static lock_graph graph;
+    vertex *v = nullptr;
+    std::mutex m;
 
 public:
     void lock();
     void unlock();
     bool try_lock();
+
+    mutex();
+    ~mutex();
 };
 
 } // namespace my
